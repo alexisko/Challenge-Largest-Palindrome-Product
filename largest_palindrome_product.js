74a2d83bd9a6ff0f11326412454cabb6d2d67d64
Alexis <alexisko@hawaii.edu>
2017-06-03 10:05:03 -1000

    First test passing!

--- largest_palindrome_product.js
+++ largest_palindrome_product.js
@@ -5,14 +5,29 @@
  *  @return {Object} an object containing the two factors used to produce
  *                   the palindromeNumber and the palindromeNumber itself.
  */
-module.exports = function(digits){
+module.exports = function (digits){
   var factor_0 = 0;
   var factor_1 = 0;
   var palindromeNumber = 0;
+  var palindromeArr = [];
+  var result = 0;
 
-  // do your work here
+  var numPlaces = [9, 99, 999, 9999, 99999];
+  var max = numPlaces[digits-1];
 
-
+  for(var i = 0; i <= max; i++) {
+    for(var j = 0; j <= max; j++) {
+      result = i * j;
+      palindromeArr = result.toString().split("");
+      if(palindromeArr.join("") === palindromeArr.reverse().join("")) {
+        if(parseInt(palindromeArr.join("")) > palindromeNumber) {
+          palindromeNumber = parseInt(palindromeArr.join(""));
+          factor_0 = i;
+          factor_1 = j;
+        }
+      }
+    }
+  }
 
   return {
     factor_0 : factor_0,
